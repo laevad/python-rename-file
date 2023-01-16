1c13dfc1c878a3ce8b95a1f7f21e68f67408cd74
Al Dave A <77134175+laevad@users.noreply.github.com>
2023-01-16 20:33:48 +0800

    ?

--- rename-copy.py
+++ rename-copy.py
@@ -2,11 +2,11 @@ import os
 import shutil
 
 # Set the input and output paths, and the list of allowed extensions
-letter = "D"
-input_path = "E:/CROPPED/cropped silt/" + letter + "-SILT"
-output_path = 'E:/CROPPED/cropped silt/CROPPEDSILT'
+letter = "C"
+DIR = "SILT"
+input_path = "E:/soilution/january/" + DIR + "/" + letter + "-" + DIR
+output_path = 'E:/soilution/january/' + DIR + '/' + DIR
 allowed_extensions = ('.jpg', '.jpeg', '.png', '.webp')
-
 # Create the output folder if it doesn't exist
 if not os.path.exists(output_path):
     os.makedirs(output_path)
@@ -19,7 +19,7 @@ for i, file in enumerate(files):
     # Check if the file has an allowed extension
     if file.endswith(allowed_extensions):
         # Generate the new file name using the alpha numeric format
-        new_name = 'image_' + letter + '_{:03d}{}'.format(i, os.path.splitext(file)[1])
+        new_name = 'image_' + DIR + '_' + letter + '_{:03d}{}'.format(i, os.path.splitext(file)[1])
 
         # Get the full paths for the old and new names
         old_name = os.path.join(input_path, file)
